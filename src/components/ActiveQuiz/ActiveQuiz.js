2cb3e3aa8df9eebc1e3196d6e029074b9c72beef
broooke <9060826518ng@gmail.com>
2021-05-13 18:44:43 +0300

    making base components

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import classes from './ActiveQuiz.module.css'
+import AnswersList from './AnswersList/AnswersList'
+
+const ActiveQuiz = props => (
+    <div className={classes.ActiveQuiz}>
+        <p className={classes.Question}>
+            <span>
+                <strong>2.</strong>&nbsp;
+                Как дела?
+            </span>
+
+            <small>4 из 12</small>
+        </p>
+
+        <AnswersList 
+            answers={props.answers}
+        />
+    </div>
+)
+
+export default ActiveQuiz
