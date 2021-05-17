fb6b34494672d30ed66502006fc503ca637a3189
broooke <9060826518ng@gmail.com>
2021-05-17 18:14:57 +0300

    commit #2

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
+
+export default class Auth extends Component {
+
+    loginHandler = () => {
+
+    }
+
+    registerHandler = () => {
+
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault()
+    }
+
+    render() {
+        return (
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        <input />
+                        <input />
+
+                        <Button type="success" onClick={this.loginHandler}>Войти</Button>
+                        <Button type="primary" onClick={this.registerHandler}>Зарегистрироваться</Button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
